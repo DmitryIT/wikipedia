41e12d46dd0a008d46e475f015c33e61a7c60ac3
dmitry <dmitry>
2018-05-11 00:51:53 +0200

    added docker-compose.yml

--- fetch_changes.py
+++ fetch_changes.py
@@ -1,5 +1,8 @@
 import json
 from sseclient import SSEClient as EventSource
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch([{'host':'localhost', 'port':9200}])
 
 def fetch_changes():
     url = 'https://stream.wikimedia.org/v2/stream/recentchange'
@@ -12,7 +15,9 @@ def fetch_changes():
             else:
                 #print('bot? {bot}, {user} edited {title}'.format(**change))
                 if change['bot'] == False:
-                    print(json.dumps(change, indent=4))
+                    #print(json.dumps(change, indent=4))
+                    res = es.index(index="wiki", doc_type="change", body=change)
+                    print(res['result'])
 
 if __name__ == '__main__':
     fetch_changes()
